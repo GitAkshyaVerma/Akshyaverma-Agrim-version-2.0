d89a57e9ee60731d4404633b3ad9ad4e7c25c7be
Ashutosh Mishra <mashutosh0221@gmail.com>
2026-05-15 00:33:09 +0530

    href fixed in mohan

--- src/pages/TopistoPage.jsx
+++ src/pages/TopistoPage.jsx
@@ -90,13 +90,13 @@ const TopistoPage = () => {
         {/* Header */}
         <header className="topisto-header">
           <div className="header-back">
-            <Link to="/" className="btn-back">
+            <Link to="/products" className="btn-back">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
               <span className="back-text">Back to Portfolio</span>
             </Link>
           </div>
           <div className="header-left">
-            <Link to="/">
+            <Link to="/topisto">
               <img src="/assets/topisto/mohan_logo.png" alt="Mohan Topisto Logo" className="brand-logo" />
             </Link>
           </div>
